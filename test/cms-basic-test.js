24f8008886c3a39eb9191240abe508a064ad09e8
Raul P <raul.popescu@gmail.com>
2018-05-27 13:43:56 +0300

    initial structure

---
+++ test/cms-basic-test.js
@@ -0,0 +1,17 @@
+todo test if:
+- can save post
+- can update post
+- can see un-published posts
+- can save / see post history
+- url generation from title
+- category support: can see post in category, can't see same post without category
+- rename post title: url remains same?
+- can change url of unpublished posts
+- 
+
+
+v2: FILE upload and management:
+ - upload file, get link to it.
+ - remove file?
+ - file management?
+ - save to: disk / aws / other?
